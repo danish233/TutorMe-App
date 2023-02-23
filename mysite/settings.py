ee009441646268fcffea92ccff1c81012dfd926b
Jayanth Peetla <ynv7jk@virginia.edu>
2023-02-23 17:31:40 -0500

    Completed Initial GoogleAuth Code

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "oauth_app",
+    'myapp',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
