a611d4efbca2daa8f288e57939d3f242abfdb011
Jacob McDaniel <jacob101mcd@gmail.com>
2023-03-19 18:19:18 -0400

    Added a few more test cases
    added a few url response test cases

--- myapp/tests.py
+++ myapp/tests.py
@@ -6,17 +6,22 @@ from myapp.models import Test
 
 
 # Create your tests here.
+<<<<<<< Updated upstream
 def setUP(title='test', body='testing'):
     return Test.objects.create(title=title, body=body, created_at=timezone.now())
 
 
 class TestModelTests(TestCase):
+=======
+class TestURLTests(TestCase):
+>>>>>>> Stashed changes
     def test_test_systems(self):
         """
-        Test case used for github CI/CD actions testing
+        Test case used for github CI/CD actions testing, and for any setup before other test cases run.
         """
         self.assertIs(True, True)
 
+<<<<<<< Updated upstream
     def test_creation(self):
         t = setUP()
         self.assertTrue(isinstance(t, Test))
@@ -31,3 +36,12 @@ class TestModelTests(TestCase):
 
 
 
+=======
+    def test_redirect_url_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_admin_url_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
+>>>>>>> Stashed changes
