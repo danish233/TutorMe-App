0c7f1fa558563979f8ae1502a2821b519e499d4b
hannahypark <hyp2ftn@virginia.edu>
2023-03-19 11:52:47 -0400

    addded pagination and display classes available

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.contrib.sites import requests
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 import requests
 
 
@@ -19,15 +20,25 @@ def index(request):
         spring_2023 = '1232'
         fall_2023 = '1238'
 
-        url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=' + spring_2023 + '&page=1'
+        dept_url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearchOptions?institution=UVA01&term=' + spring_2023
+        req = requests.get(dept_url)
+        #clist = req.json()['acad_groups']
+        clist = ['MATH', 'ENGR', 'APMA']
 
-        r = requests.get(url + '&subject=' + 'MATH')
-        classes = []
-        for c in r.json():
-            class_info = c['catalog_nbr'] + '-' + c['class_section'], c['component'] + c['descr']
-            classes.append(class_info)
+        url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=' + spring_2023
 
-        context = {'classes': classes}
+        for s in clist:
+            r = requests.get(url + '&subject=' + str(s) + '&page=1')
+            classes = []
+            for c in r.json():
+                class_info = c['catalog_nbr'] + '-' + c['class_section'], c['component'] + c['descr']
+                classes.append(class_info)
+
+        paginator = Paginator(classes, 25)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context = {'page_obj': page_obj}
+        #context = {'classes': classes}
         return render(request, 'index.html', context)
 
 
