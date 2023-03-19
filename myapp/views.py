1d10f68a1eeed3f3395d8aa690406f6d8069d639
hannahypark <hyp2ftn@virginia.edu>
2023-03-19 13:23:50 -0400

    added pagination and display classes available

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 import requests
+import json
 
 
 @login_required
@@ -22,14 +23,17 @@ def index(request):
 
         dept_url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearchOptions?institution=UVA01&term=' + spring_2023
         req = requests.get(dept_url)
-        #clist = req.json()['acad_groups']
-        clist = ['MATH', 'ENGR', 'APMA']
+        #name = req.json()['subjects']
+        #clist = []
+        #for i in name:
+            #clist.append(i['subject'])
+
+        clist = ['MATH', 'APMA', 'ENGR']
 
         url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=' + spring_2023
-
+        classes = []
         for s in clist:
-            r = requests.get(url + '&subject=' + str(s) + '&page=1')
-            classes = []
+            r = requests.get(url + '&subject=' + s + '&page=1')
             for c in r.json():
                 class_info = c['catalog_nbr'] + '-' + c['class_section'], c['component'] + c['descr']
                 classes.append(class_info)
