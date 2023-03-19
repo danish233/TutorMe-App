f7de2214065bcd43cc9daee599b927ac987d4a02
Jacob McDaniel <jacob101mcd@gmail.com>
2023-03-19 18:24:12 -0400

    Added a few more test cases
    added a few url response test cases

--- myapp/tests.py
+++ myapp/tests.py
@@ -8,3 +8,11 @@ class TestModelTests(TestCase):
         Test case used for github CI/CD actions testing
         """
         self.assertIs(True, True)
+
+    def test_redirect_url_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_admin_url_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 302)
