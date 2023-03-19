5e35bdf559efaef250ceb47849a3fe9e0113cffe
Glory Gurrola <glorygurrola@Glorys-MacBook-Pro.local>
2023-03-19 16:12:25 -0400

    writing testing views

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Test(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.Textfield()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
--- myapp/tests.py
+++ myapp/tests.py
@@ -2,8 +2,14 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from views import index, student, tutor
 from django.utils import timezone
+from myapp.models import Test
+
 
 # Create your tests here.
+def setUP(title='test', body='testing'):
+    return Test.objects.create(title=title, body=body, created_at=timezone.now())
+
+
 class TestModelTests(TestCase):
     def test_test_systems(self):
         """
@@ -11,10 +17,10 @@ class TestModelTests(TestCase):
         """
         self.assertIs(True, True)
 
-    def setUP(self):
+    def test_creation(self):
+        t = setUP()
+        self.assertTrue(isinstance(t, Test))
 
-    def test_accounts_URls(self):
-        url = reverse('index')
-        print(resolve(url))
-        self.assertEqual()
-
+    def test_views(self):
+        v = self.create_index()
+        url = reverse('')
