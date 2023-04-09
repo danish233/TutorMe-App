b608412f7026e1fe96ab7b2dc2f830913b8e4517
danish233 <dic2fx@virginia.edu>
2023-04-09 17:03:17 -0400

    Fixed slow speed for tutor and student

--- myapp/views.py
+++ myapp/views.py
@@ -89,36 +89,33 @@ def student(request):
 
 @login_required
 def tutor(request):
-    # spring_2023 = '1232'
-    #
-    # dept_url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearchOptions?institution=UVA01&term=' + spring_2023 + '&career=UGRD&institutions=UVA01'
-    # req = requests.get(dept_url)
-    # name = req.json()['subjects']
-    # clist = [i['subject'] for i in name]
-    #
-    # url = 'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=' + spring_2023
-    classes = []
-    search_query = request.GET.get('search_query')
-    # if search_query:
-    #     for s in clist:
-    #         r = requests.get(url + '&subject=' + s + '&page=1')
-    #         for c in r.json():
-    #             if search_query.lower() in c['descr'].lower() or search_query.lower() in c['catalog_nbr'].lower():
-    #                 class_info = c['catalog_nbr'] + '-' + c['class_section'], c['component'] + c['descr']
-    #                 classes.append(class_info)
-    # else:
-    #     for s in clist:
-    #         r = requests.get(url + '&subject=' + s + '&page=1')
-    #         for c in r.json():
-    #             class_info = c['catalog_nbr'] + '-' + c['class_section'], c['component'] + c['descr']
-    #             classes.append(class_info)
-
-    paginator = Paginator(classes, 25)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {'page_obj': page_obj, 'search_query': search_query}
-    # return render(request, 'index.html', context)
-    return render(request, 'tutor.html', context)
+    if 'q' in request.GET:
+        query = request.GET['q']
+        query = str(query)
+        term = '1232'  # replace with appropriate term code
+        url = f'https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term={term}&keyword={query}'
+        response = requests.get(url)
+        data = response.json()
+        courses = []
+        for c in data:
+            if query.lower() in c['subject'].lower() or query.lower() in c['catalog_nbr'].lower() or \
+               query.lower() in c['class_section'].lower() or query.lower() in c['component'].lower() or \
+               query.lower() in c['descr'].lower() or query.lower() in str(c['class_nbr']):
+                course = {
+                    'subject': c['subject'],
+                    'catalog_nbr': c['catalog_nbr'],
+                    'class_section': c['class_section'],
+                    'component': c['component'],
+                    'descr': c['descr'],
+                    'class_nbr': c['class_nbr'],
+                    'class_capacity': c['class_capacity'],
+                    'enrollment_available': c['enrollment_available']
+                }
+                courses.append(course)
+        context = {'courses': courses, 'query': query}
+        return render(request, 'tutor.html', context)
+    else:
+        return render(request, 'tutor.html')
 
 from django.shortcuts import render
 from .models import TutorClass
